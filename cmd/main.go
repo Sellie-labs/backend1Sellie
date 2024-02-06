71e15399e9b826cadabab5394bd45d6222bf4c36
Abraxas-365 <luisfmiranda8@gmail.com>
2024-02-06 10:17:36 -0500

    fixendpoints

--- cmd/main.go
+++ cmd/main.go
@@ -17,7 +17,7 @@ func main() {
 	os.Setenv("DB_USER", "postgres")
 	os.Setenv("DB_PASSWORD", "postgres")
 	os.Setenv("DB_NAME", "postgres")
-	os.Setenv("DB_HOST", "172.233.25.33") // Use your database IP address
+	os.Setenv("DB_HOST", "localhost") // Use your database IP address
 	os.Setenv("DB_PORT", "5432")
 
 	// Initialize database connection pool and run migrations
@@ -28,7 +28,7 @@ func main() {
 	db := database.GetPool() // Retrieve the initialized database pool
 	// Initialize repository, AI chat adapter, and chat service
 	repo := adapter.NewSQLChatSessionRepository(db)
-	ai := adapter.NewChatAdapter("http://localhost") // Update the URL to your AI service if needed
+	ai := adapter.NewChatAdapter("http://localhost:8083") // Update the URL to your AI service if needed
 	service := chat.NewService(repo, ai, "EAAEccmazn6IBO0td4CEL98CA7w3NSjst4Iz85ZB7pjDbZBo7IlF2eKh9a628IJxvjMA4KmCwtZCgTTePrExe58LZB1szK4VDlpjZBoo0TY61n75MQKuAL93romYWGb9VfmQEpNPULipanwAagdds34V4BpsifWDPWo3rviSgMR8vPaQCKuonUcNHOZASrQk7qgGlq7KEpqU43CYJBiCPqX1AZDZD")
 
 	// Setup HTTP server using Fiber
